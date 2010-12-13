bbc115dd6d14792442807a5cba2a72322dd6712e
Brandon Lewis <brandon_lewis@alum.berkeley.edu>
2010-12-13 17:12:54 +0000

    Main: instantiate QApplicaiton instance

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #define GST_USE_UNSTABLE_API
 
 #include <ges/ges.h>
+#include <QtGui>
 
 int main(int argc, char **argv)
 {
+  QApplication app(argc, argv);
   gst_init(&argc, &argv);
   ges_init();
   return 0;
