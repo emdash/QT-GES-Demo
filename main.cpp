137cbc301517bb9602657c4f77bfcbeacf7ab5d5
Brandon Lewis <brandon_lewis@alum.berkeley.edu>
2010-12-14 17:29:16 +0000

    Main: register the new type with qml

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #define GST_USE_UNSTABLE_API
 
 #include <ges/ges.h>
+#include <QtCore>
 #include <QtGui>
 #include <QtDeclarative>
+#include "timeline.h"
 
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
   gst_init(&argc, &argv);
   ges_init();
-  
+
+  qmlRegisterType<Timeline>("GES", 1, 0, "GESTimeline");
+
   QDeclarativeView view;
   view.setSource(QUrl::fromLocalFile("Timeline.qml"));
   view.show();
