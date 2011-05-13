9f3f01738daa3a86afca13cf7d3f9bc7f92756ea
Brandon Lewis <brandon_lewis@alum.berkeley.edu>
2011-05-13 14:09:32 -0700

    Add a script do download media files

---
+++ download.sh
@@ -0,0 +1,18 @@
+mkdir -p media/thumbnails
+pushd media
+wget -N http://people.collabora.co.uk/~brandon/media/20100510_007.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/20100512_003.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/Caterpilla_345C_Longfront_01.ogv
+wget -N http://people.collabora.co.uk/~brandon/media/Merkava_III_Showoff.ogg
+wget -N http://people.collabora.co.uk/~brandon/media/small-mvi_0009.avi
+wget -N http://people.collabora.co.uk/~brandon/media/SOURCES
+wget -N http://people.collabora.co.uk/~brandon/media/Typing_example.ogv
+pushd thumbnails
+wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/20100510_007.png
+wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/20100512_003.png
+wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/mid-Caterpilla_345C_Longfront_01.ogv.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/mid-Merkava_III_Showoff.ogg.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/mid-Typing_example.ogv.
+wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/small-mvi_0009.png
+popd
+popd
