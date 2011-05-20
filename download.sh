68704a58422f5dedc4e1145eb7a1f74e73a90685
Brandon Lewis <brandon_lewis@alum.berkeley.edu>
2011-05-20 14:09:42 -0700

    Update downloader

--- download.sh
+++ download.sh
@@ -1,18 +1,43 @@
 mkdir -p media/thumbnails
 pushd media
-wget -N http://people.collabora.co.uk/~brandon/media/20100510_007.jpg
-wget -N http://people.collabora.co.uk/~brandon/media/20100512_003.jpg
-wget -N http://people.collabora.co.uk/~brandon/media/Caterpilla_345C_Longfront_01.ogv
-wget -N http://people.collabora.co.uk/~brandon/media/Merkava_III_Showoff.ogg
-wget -N http://people.collabora.co.uk/~brandon/media/small-mvi_0009.avi
-wget -N http://people.collabora.co.uk/~brandon/media/SOURCES
-wget -N http://people.collabora.co.uk/~brandon/media/Typing_example.ogv
+wget -N http://people.collabora.co.uk/~brandon/media/20100513_004.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/20100516_005.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20100516_007.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20100516_015.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/20100525_003.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20100818_006.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20100818_011.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20101011_001.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20101105_002.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20110101_002.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20110101_003.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20110101_004.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20110101_006.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20110101_007.mp4.avi
+wget -N http://people.collabora.co.uk/~brandon/media/20110202_008.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/20110202_023.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/20110216_004.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/20110218_010.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/20110227_002.jpg
 pushd thumbnails
-wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/20100510_007.png
-wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/20100512_003.png
-wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/mid-Caterpilla_345C_Longfront_01.ogv.jpg
-wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/mid-Merkava_III_Showoff.ogg.jpg
-wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/mid-Typing_example.ogv.
-wget -N http://people.collabora.co.uk/~brandon/media/thumbnails/small-mvi_0009.png
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20100513_004.jpg.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20100516_005.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20100516_007.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20100516_015.jpg.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20100525_003.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20100818_006.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20100818_011.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20101011_001.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20101105_002.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110101_002.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110101_003.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110101_004.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110101_006.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110101_007.mp4.avi.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110202_008.jpg.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110202_023.jpg.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110216_004.jpg.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110218_010.jpg.jpg
+wget -N http://people.collabora.co.uk/~brandon/media/thumbs/20110227_002.jpg.jpg
 popd
 popd
